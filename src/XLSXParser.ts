393e420b748bbb12e50cf54c76bc37e8079b43d4
hayabusabusa <hayabusabusaSY@gmail.com>
2024-08-01 23:54:30 +0900

    feat: xlsx ファイルからデータを 1 つずつ抜き出す実装を追加

---
+++ src/XLSXParser.ts
@@ -0,0 +1,72 @@
+import * as XLSX from "xlsx";
+
+/**
+ * `xlsx` ファイルの座標.
+ */
+export interface XLSXCoordinate {
+  /**
+   * X 座標.
+   */
+  x: string;
+  /**
+   * Y 座標.
+   */
+  y: number
+}
+
+export type XLSXCallback = (coordinate: XLSXCoordinate, value: string) => void;
+
+/**
+ * XSLXファイルパース時にアクセスする情報などをまとめたモデル.
+ */
+export interface XLSXParseConfiguration {
+  /**
+   * 読み込むXSLXファイルのパス.
+   */
+  sheetPath: string
+
+  /**
+   * 座標ごとのデータを取得した際に呼び出されるコールバック.
+   */
+  callback?: XLSXCallback
+}
+
+/**
+ * `xlsx` ファイルからデータを抜き出すクラス.
+ */
+export class XLSXParser {
+  /**
+   * 指定された `xlsx` ファイルからデータを抜き出す.
+   * @param configuration 読み込むファイルの設定など.
+   */
+  parse(configuration: XLSXParseConfiguration) {
+    const book = XLSX.readFile(configuration.sheetPath);
+    const sheet = book.Sheets[book.SheetNames.at(0) ?? ""];
+
+    // xlsx ファイルの最後までの範囲を取得.
+    const range = XLSX.utils.decode_range(sheet['!ref']!);
+    const endOfFileRow = range.e.r;
+    const endOfFileColumn = range.e.c;
+
+    // 座標 1 つずつにアクセスして値を取得.
+    for (let row = range.s.r; row <= endOfFileRow; row++) {
+      for (let column = range.s.c; column <= endOfFileColumn; column++) {
+        const address: XLSX.CellAddress = { 
+          c: column, 
+          r: row 
+        };
+        // `A1` のような形式に変換する.
+        const reference = XLSX.utils.encode_cell(address);
+        const cell = sheet[reference];
+
+        if (cell && cell.v !== undefined) {
+          const coordinate: XLSXCoordinate = {
+            x: XLSX.utils.encode_col(column),
+            y: row
+          }
+          configuration.callback?.(coordinate, cell.v);
+        }
+      }
+    }
+  }
+}
