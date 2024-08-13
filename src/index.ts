66b5483eef2a7598fcb9e0a772833ff369c064ee
hayabusabusa <hayabusabusaSY@gmail.com>
2024-08-13 16:56:34 +0900

    feat: スクリプト実行時の処理を修正

--- src/index.ts
+++ src/index.ts
@@ -1,22 +1,8 @@
-import * as XLSX from "xlsx";
+import { TimetableUseCase } from "./useCase";
 
-import { CalendarParser } from "./calendarParser";
-import { outputJSON } from "./output";
-
-const main = () => {
-  const book = XLSX.readFile("resource/calendar.xlsx");
-  const sheetName = book.SheetNames.at(0);
-  
-  if (sheetName == null) {
-    console.log("Sheet name not found.");
-    return;
-  }
-
-  const sheet = book.Sheets[sheetName];
-  const parser = new CalendarParser();
-  const calendars = parser.parse(sheet);
-
-  outputJSON(calendars, "calendar");
+async function main() {
+  const timetableUseCase = new TimetableUseCase();
+  await timetableUseCase.execute();
 };
 
 main();
