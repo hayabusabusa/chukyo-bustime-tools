16b59de6092abda62cbd9dcac372575391bef608
Shunya Yamada <hayabusabusaSY@gmail.com>
2025-03-18 00:10:59 +0900

    fix: カレンダーのパース時に渡す設定を定数化

--- src/types/CalendarXLSXParseConfiguration.ts
+++ src/types/CalendarXLSXParseConfiguration.ts
@@ -36,3 +36,55 @@ export interface CalendarXLSXParseConfiguration extends XLSXParseConfiguration {
      */
     specialDiagramValues: string[];
 }
+
+/**
+ * 2025 年のカレンダーの `xlsx` ファイルパースのデフォルト設定.
+ */
+export const calendarXLSXParseDefaultConfiguration: CalendarXLSXParseConfiguration = {
+    sheetPath: 'resource/calendar.xlsx',
+    startCoordinate: {
+        x: 'A',
+        y: 3,
+    },
+    endCoordinate: {
+        x: 'O',
+        y: 57,
+    },
+    year: 2025,
+    month: 3,
+    dateCoordinates: [
+        'B',
+        'D',
+        'F',
+        'H',
+        'J',
+        'L',
+        'N',
+    ],
+    diagramCoordinates: [
+        'C',
+        'E',
+        'G',
+        'I',
+        'K',
+        'M',
+        'O',
+    ],
+    bDiagramValues: [
+        '卒業式（予定）',
+        'Ｂ高大接続入試(現社)',
+        '卒業式（予定）',
+    ],
+    cDiagramValues: [
+        '梅村学園創立記念日  C',
+        'C高大接続入試(スポーツ)',
+    ],
+    specialDiagramValues: [
+        '教育懇談会（豊田予定）',
+        'オープンキャンパス',
+        '定期試験',
+        '高大連携授業',
+        '附属高校体育祭',
+        '大学祭',
+    ],
+}; 
--- src/useCase/CalendarUseCase.ts
+++ src/useCase/CalendarUseCase.ts
@@ -1,6 +1,7 @@
 import { CalendarFormatter } from "../formatter"
 import { JSONOutput } from "../output";
 import { CalendarXSLXParser } from "../parser";
+import { calendarXLSXParseDefaultConfiguration } from "../types";
 
 export class CalendarUseCase {
     async execute() {
@@ -8,54 +9,7 @@ export class CalendarUseCase {
         const formatter = new CalendarFormatter();
         const parser = new CalendarXSLXParser(formatter);
 
-        const calendars = parser.parseForCalendar({
-            sheetPath: 'resource/calendar.xlsx',
-            startCoordinate: {
-                x: 'A',
-                y: 3,
-            },
-            endCoordinate: {
-                x: 'O',
-                y: 57,
-            },
-            year: 2025,
-            month: 3,
-            dateCoordinates: [
-                'B',
-                'D',
-                'F',
-                'H',
-                'J',
-                'L',
-                'N',
-            ],
-            diagramCoordinates: [
-                'C',
-                'E',
-                'G',
-                'I',
-                'K',
-                'M',
-                'O',
-            ],
-            bDiagramValues: [
-                '卒業式（予定）',
-                'Ｂ高大接続入試(現社)',
-                '卒業式（予定）',
-            ],
-            cDiagramValues: [
-                '梅村学園創立記念日  C',
-                'C高大接続入試(スポーツ)',
-            ],
-            specialDiagramValues: [
-                '教育懇談会（豊田予定）',
-                'オープンキャンパス',
-                '定期試験',
-                '高大連携授業',
-                '附属高校体育祭',
-                '大学祭',
-            ],
-        });
+        const calendars = parser.parseForCalendar(calendarXLSXParseDefaultConfiguration);
 
         await jsonOutput.output(calendars, 'calendars');
     }
